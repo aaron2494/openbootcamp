3236c84851c895eb1db2a6e6beee88edcf73bfde
aaron2494 <aaron.e.francolino@gmail.com>
2022-09-05 19:15:24 -0300

    ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,43 @@
+let listaCompras=[
+    "coca",
+    "tomates",
+    "queso",
+    "salame",
+    "papa"
+]
+listaCompras.push("aceite de girasol");
+listaCompras.pop();
+
+
+const peliculas = [
+    {
+        titulo: "El efecto mariposa",
+        director: "Eric Bress",
+        fecha: new Date(2004, 3, 30)
+    },
+    {
+        titulo: "Spider-Man: No Way Home",
+        director: "Jon Watts",
+        fecha: new Date(2021, 11, 16)
+    },
+    {
+        titulo: "Pulp Fiction",
+        director: "Quentin Tarantino",
+        fecha: new Date(1995, 0, 13)
+    }
+]
+const pelisNuevas=peliculas.filter(pelicula=>pelicula.fecha > new Date(2010,0,1))
+
+const directores =peliculas.map(pelicula=>{
+    return pelicula.director
+})
+
+const titulos =peliculas.map(pelicula=>{
+    return pelicula.titulo;
+})
+
+const directores_titulos =directores.concat(titulos)
+
+const directores_titulos_prop=[...directores, ...titulos];
+console.log(directores_titulos_prop)
+
