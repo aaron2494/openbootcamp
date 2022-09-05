f781c20d8c1e70aaf9d0cddc713eb0bbc87a9391
aaron2494 <aaron.e.francolino@gmail.com>
2022-09-05 13:31:54 -0300

    ejercicio2

---
+++ js/ejercicio2.js
@@ -0,0 +1,13 @@
+
+const fechaN= new Date("april 24 1994")
+
+const libroFavorito={
+nombre:"asi hablo zaratustra",
+autor:"nieztche",
+fecha:"1960",
+URL:""
+}
+
+const miPersona=["aaron francolino",28,true,fechaN, libroFavorito]
+
+console.log(miPersona);
