f0bb231965bdbbb0d56ac9f8107d02cb508be9b0
aaron2494 <aaron.e.francolino@gmail.com>
2022-09-05 18:13:42 -0300

    ejercicio5

---
+++ js/ejercicio5.js
@@ -0,0 +1,11 @@
+
+const altura_cm = 185
+const altura_m = 1.85
+const peso_kg = 79.5
+
+const altura_red = Math.ceil(altura_m)
+const peso_red = Math.floor(peso_kg)
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(sonIguales)
